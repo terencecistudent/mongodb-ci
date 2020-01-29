00506b8aa07e993d9d9927190ac30b9345861595
terencecistudent <terencelogue100@hotmail.co.uk>
2020-01-29 17:04:52 +0000

    CRUD Operations MongoDB and Python3

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ MONGODB_URI = os.getenv("MONGO_URI")
 DBS_NAME = "myTestDB"
 COLLECTION_NAME = "myFirstMDB"
 
+
 def mongo_connect(url):
     try:
         conn = pymongo.MongoClient(url)
@@ -18,13 +19,14 @@ def mongo_connect(url):
     except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to MongoDB: %s") % e
 
+
 conn = mongo_connect(MONGODB_URI)
 
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
 coll.update_many({'nationality': 'american'}, {'$set': {'hair_colour': 'maroon'}})
 
-documents = coll.find({'nationality': 'american'})
+documents = coll.find()
 
 for doc in documents:
     print(doc)
